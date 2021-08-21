ac13e2344136f553da4546de9150849d05a86ed0
bassantt <bassantmohamed945@yahoo.com>
2021-08-22 01:50:36 +0200

    basic pages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "normalize.css";
+import Vuelidate from "vuelidate";
+import axios from "axios";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+Vue.use(Vuelidate);
+Vue.prototype.$http = axios;
+var vm = new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
+
+global.vm = vm;
